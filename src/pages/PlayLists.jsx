5dca2adcf45727300abc98e9e433c168b6c84f44
Sameer <gobindsinghal87@gmail.com>
2025-06-20 18:47:55 +0530

    correcting the ui of tweet

--- src/pages/PlayLists.jsx
+++ src/pages/PlayLists.jsx
@@ -126,7 +126,6 @@ function PlayLists() {
                 onClick={() => {
                   handleDeletePlaylist(playlistToDelete);
                   setShowDeleteModal(false);
-                  window.location.reload();
                 }}
               >
                 Delete
